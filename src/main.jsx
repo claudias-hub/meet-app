d0e79e2953bdf079e7ed5bdf5054e13056c53997
claudias-hub <c_oka82@hotmail.com>
2025-07-21 13:59:46 +0200

    Atatus integrated successfully

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as atatus from 'atatus-spa';
+
+atatus.config('4e425f05f3f644eda2bf4332242f8537').install();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
