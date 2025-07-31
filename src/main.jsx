3a73675e1ba6a4ffde5a803267876178867ed9f8
claudias-hub <c_oka82@hotmail.com>
2025-07-31 15:43:21 +0200

    Import the service worke in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import * as atatus from 'atatus-spa';
+import './index.css'
+// import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-atatus.config('4e425f05f3f644eda2bf4332242f8537').install();
+// atatus.config('4e425f05f3f644eda2bf4332242f8537').install();
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
+serviceWorkerRegistration.register();
 
